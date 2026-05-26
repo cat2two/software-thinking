f680d62dec9eb365d31c7adb22e293af929fbf94
zunbiligk <zunbiligk@gmail.com>
2026-05-26 14:33:41 +0900

    Update main_not_gui_admin.py

--- main_not_gui_admin.py
+++ main_not_gui_admin.py
@@ -1,2 +1,14 @@
-with open("stock.txt", "a", encoding="utf-8") as f:
-    f.write(f"{input()}\n")
+choice = input("1=plus 2=minus: ")
+
+if choice == "1":
+    with open("stock.txt", "a", encoding="utf-8") as f:
+        f.write(f"{input()}\n")
+
+elif choice == "2":
+    target = input()
+    with open("stock.txt", "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    with open("stock.txt", "w", encoding="utf-8") as f:
+        for line in lines:
+            if line.strip() != target:
+                f.write(line)
